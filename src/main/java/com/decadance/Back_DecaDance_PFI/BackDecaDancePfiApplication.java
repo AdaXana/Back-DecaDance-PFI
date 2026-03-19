0587b179a4e3b8750addd3ae83c47d5abecbdc3f
Guadalupe <penaegeaguadalupe@gmail.com>
2026-03-19 11:45:07 +0100

    chore: setup initialized spring boot add dependencies at pom and conected with DDBB decadance

---
+++ src/main/java/com/decadance/Back_DecaDance_PFI/BackDecaDancePfiApplication.java
@@ -0,0 +1,13 @@
+package com.decadance.Back_DecaDance_PFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDecaDancePfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDecaDancePfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decadance/Back_DecaDance_PFI/BackDecaDancePfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decadance.Back_DecaDance_PFI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDecaDancePfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

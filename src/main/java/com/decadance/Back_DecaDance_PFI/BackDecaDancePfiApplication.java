fb319e0d108322de0e468b6829e4b8d4bbc6438a
Guadalupe <penaegeaguadalupe@gmail.com>
2026-03-23 13:36:11 +0100

    refactor: extract initial data seeding to DataSeeder config

--- src/main/java/com/decadance/Back_DecaDance_PFI/BackDecaDancePfiApplication.java
+++ src/main/java/com/decadance/Back_DecaDance_PFI/BackDecaDancePfiApplication.java
@@ -2,9 +2,6 @@ package com.decadance.Back_DecaDance_PFI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.boot.CommandLineRunner;
-// import com.decadance.Back_DecaDance_PFI.service.MusicImportService;
 
 @SpringBootApplication
 public class BackDecaDancePfiApplication {
@@ -12,12 +9,5 @@ public class BackDecaDancePfiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackDecaDancePfiApplication.class, args);
 	}
-
-	// @Bean
-	// CommandLineRunner initData(MusicImportService musicImportService) {
-	// 	return args -> {
-	// 		musicImportService.importSongsFromCsv();
-	// 	};
-	// }
 
 }
